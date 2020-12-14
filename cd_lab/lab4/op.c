ff73bc674f0bb8a220eca12f3051778c2043d3c9
aadideshpande <aadi.deshpande1@gmail.com>
2020-12-14 16:45:06 +0530

    lab8 incomplete

---
+++ cd_lab/lab4/op.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+
+int sum(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+
+int main()
+{
+	int x, y;
+	printf("enter the two numbers \n");
+	scanf("%d %d", &x, &y);
+	printf("sum is %d \n", sum(a, b));
+
+}
+
